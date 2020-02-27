b5b274427acc5158cf804daf7835563a804ff9c6
rajmeet2001 <rajmeet2001@gmail.com>
2020-02-26 21:30:49 -0600

    Created Skeleton

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-alert('Hello Name!')
+
+//chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {  //gives access to all data on page
+//    const re = new RegExp('hello', 'gi') //case insensitive looks for all instances of hello
+//    const matches = document.documentElement.innerHTML.match(re)
+//    sendResponse({count: matches.length})
+//})
+
+const re = new RegExp('hello', 'gi') //case insensitive looks for all instances of hello
+const matches = document.documentElement.innerHTML.match(re) || []
+
+chrome.runtime.sendMessage({
+    url: window.location.href,
+    count: matches.length
+})
